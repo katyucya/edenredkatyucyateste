30ad7fc312a911843660cf72f1998049094ca412
katyucya <katyucyafabiane@gmail.com>
2021-06-14 00:33:45 -0400

    Add files via upload

--- src/test/java/com/herokuapp/edenredkatyucyateste/JPATest.java
+++ src/test/java/com/herokuapp/edenredkatyucyateste/JPATest.java
@@ -1,8 +1,8 @@
 package com.herokuapp.edenredkatyucyateste;
+ 
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
@@ -26,10 +26,39 @@ class JPATest {
 		Email emailBusca=interEmailDAO.findOne((Example<Email>) e).orElse(new Email());
 	     
 	    // then
-	    assertThat(e.getNome())
-	      .isEqualTo(emailBusca.getNome());
+	    Assertions.assertEquals(e.getNome(),emailBusca.getNome());
 	}
 	
+	@Test
+	void testeAlterar() {
+		Email e=new Email();
+		e.setNome("Fulano");
+		e.setCorreioEmail("fulano@gmail.com");
+		interEmailDAO.save(e);
+		Email emailBusca=interEmailDAO.findOne((Example<Email>) e).orElse(new Email());
+		emailBusca.setNome("Ciclano");
+		interEmailDAO.save(emailBusca);
+		Email emailBuscaSalvo=interEmailDAO.findOne((Example<Email>) e).orElse(new Email());
+		 Assertions.assertEquals(emailBuscaSalvo.getNome(),emailBusca.getNome());
+				 
+	    
+	}
 	
+	@Test
+	void testeDeletar() {
+		Email e=new Email();
+		e.setNome("Katyucya");
+		e.setCorreioEmail("katyucya@gmail.com");
+		
+		interEmailDAO.save(e);
+		
+		Email emailBusca=interEmailDAO.findOne((Example<Email>) e).orElse(new Email());
+	      
+		interEmailDAO.deleteById(emailBusca.getIdEmail());
+		
+		Email deletado= interEmailDAO.findOne((Example<Email>) e).orElse(new Email());
+	    // then
+		 Assertions.assertNull(deletado.getIdEmail());
+	}
 
 }
