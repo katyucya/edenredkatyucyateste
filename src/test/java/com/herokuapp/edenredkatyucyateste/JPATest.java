2ec5becb2728fcf854028e8588adece3afa76009
katyucya <katyucyafabiane@gmail.com>
2021-06-14 00:18:35 -0400

    Add files via upload

--- src/test/java/com/herokuapp/edenredkatyucyateste/JPATest.java
+++ src/test/java/com/herokuapp/edenredkatyucyateste/JPATest.java
@@ -3,12 +3,14 @@ package com.herokuapp.edenredkatyucyateste;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.herokuapp.edenredkatyucyateste.dao.inter.InterEmailDAO;
 import com.herokuapp.edenredkatyucyateste.entity.Email;
-
+@RunWith(SpringRunner.class)
 class JPATest {
 	@Autowired
 	InterEmailDAO interEmailDAO;
